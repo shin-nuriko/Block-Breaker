d88be94d98ad6942127a5970424d6d67ffe2522d
shin-nuriko <nuriko@Hyacynths-MBP.home>
2016-05-26 13:15:12 +0800

    add smoke effect on bricks destroyed

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,21 +6,24 @@ public class Brick : MonoBehaviour {
 	public AudioClip crack;
 	public Sprite[] hitSprites;
 	public static int breakableCount = 0;
+	public GameObject smoke;
 	
 	private int timesHit;
 	private LevelManager levelManager;
 	private bool isBreakable;
+	
 	// Use this for initialization
 	void Start () {
 		timesHit = 0;
 		isBreakable = (this.tag == "Breakable");
 		//keep track of breakable bricks
 		if (isBreakable) {
-			breakableCount++;
-		}
-		
+			breakableCount++;				
+		}		
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
+	
+
 	
 	void OnCollisionEnter2D(Collision2D collision) {
 		AudioSource.PlayClipAtPoint( crack, transform.position, 0.1f );
@@ -34,11 +37,17 @@ public class Brick : MonoBehaviour {
 		timesHit++;				
 		if (timesHit >= maxHits) {
 			breakableCount--;
+			puffSmoke();
 			Destroy(gameObject);
 			levelManager.BrickDestroyed();
 		} else {
 			LoadSprites();
 		}
+	}
+	
+	void puffSmoke() {
+		GameObject smokePuff = Instantiate(smoke, gameObject.transform.position, Quaternion.identity) as GameObject;//instantiate just returns Object not GameObject
+		smokePuff.particleSystem.startColor = this.GetComponent<SpriteRenderer>().color;
 	}
 	
 	void LoadSprites () {
